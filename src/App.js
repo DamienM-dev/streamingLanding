83b835af5fb7ac388110ce66e894fafa60b27aa9
Damien MIREMONT <damien.miremont@gmail.com>
2024-01-09 10:46:56 +0100

    récupération et affichage du nom des ids genres

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const seperator = " | ";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [genres, setGenres] = useState([]);
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -41,9 +42,34 @@ function App() {
     fetchMovies();
   }, []);
 
+  useEffect(() => {
+    const fetchGenres = async () => {
+      try {
+        const res = await fetch(
+          "https://api.themoviedb.org/3/genre/movie/list?api_key=4a27561cb4efc07bd11f3574a64dbf50&language=en-EN"
+        );
+
+        if (!res.ok) {
+          throw new Error(`Erreur HTTP :${res.status}`);
+        }
+        const dataGenres = await res.json();
+
+        const genresMap = dataGenres.genres.reduce((acc, genre) => {
+          acc[genre.id] = genre.name;
+          return acc;
+        }, {});
+
+        setGenres(genresMap);
+      } catch (error) {
+        console.log("une erreur est survenue:", error);
+      }
+    };
+    fetchGenres();
+  }, []);
+
   return (
     <main>
-      <div className="p-3 bg-[url('https://image.tmdb.org/t/p/w500/xWT5F1DNxciNLEMXRl49iq8zvN7.jpg')] ">
+      <div className="p-3 bg-[url('https://image.tmdb.org/t/p/w500/xWT5F1DNxciNLEMXRl49iq8zvN7.jpg')] bg-cover">
         {movies.map((movie, index) => (
           <>
             <Navigation />
@@ -54,7 +80,17 @@ function App() {
                 <p>{Math.round(movie.popularity) / 10}/10</p>
               </div>
               <h2 className="uppercase my-2 text-2xl">{movie.title}</h2>
-              <p className="text-sm">Genre ID: {movie.genreId}</p>
+
+              <ul className="flex items-center text-sm">
+                {movie.genreId.map((genreId) => (
+                  <>
+                    <p className="pr-2">{seperator}</p>
+                    <li key={genreId} className="text-sm mr-2">
+                      {genres[genreId]}
+                    </li>
+                  </>
+                ))}
+              </ul>
             </div>
           </>
         ))}
